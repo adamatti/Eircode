6ee03aceacd71343f31cb955f195d5d2cdc59406
Marcelo Adamatti <adamatti@gmail.com>
2016-11-05 11:18:01 -0200

    Added spring

--- src/main/groovy/adamatti/Main.groovy
+++ src/main/groovy/adamatti/Main.groovy
@@ -1,7 +1,20 @@
 package adamatti
 
+import groovy.util.logging.Slf4j
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.GenericGroovyApplicationContext
+
+@Slf4j
 class Main {
     static void main (String [] args){
-        println "Init"
+        log.info("Starting")
+        buildSpringContext()
+        log.info("Started")
+    }
+
+    static ApplicationContext buildSpringContext(){
+        ApplicationContext ctx = new GenericGroovyApplicationContext("classpath:spring.groovy")
+        ctx.registerShutdownHook()
+        ctx
     }
 }
---
+++ src/main/resources/spring.groovy
@@ -0,0 +1,3 @@
+beans {
+    xmlns([context:"http://www.springframework.org/schema/context"])
+}
