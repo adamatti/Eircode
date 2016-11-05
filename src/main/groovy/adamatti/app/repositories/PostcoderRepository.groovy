e2473d37beda06ebd8bfd0e96d80625c9ecca3e0
Marcelo Adamatti <adamatti@gmail.com>
2016-11-05 17:03:59 -0200

    Improve logs

--- src/main/groovy/adamatti/app/repositories/PostcoderRepository.groovy
+++ src/main/groovy/adamatti/app/repositories/PostcoderRepository.groovy
@@ -1,9 +1,11 @@
 package adamatti.app.repositories
 
 import adamatti.Config
+import groovy.util.logging.Slf4j
 import groovyx.net.http.RESTClient
 import org.springframework.stereotype.Component
 
+@Slf4j
 @Component
 class PostcoderRepository {
     private def cfg = Config.cfg
@@ -11,10 +13,18 @@ class PostcoderRepository {
     Object get(String path, Map query = [:]){
         query.format = "json" //format should be aways json
 
-        getClient().get(
+        log.trace("Calling ${cfg.postcoder.baseUrl}${path}, query: ${query}")
+
+        Object result = getClient().get(
             path: path,
             query: query
         ).data
+
+        if (log.isTraceEnabled()){
+            log.trace("Response: ${result}")
+        }
+
+        result
     }
 
     private RESTClient getClient(){
